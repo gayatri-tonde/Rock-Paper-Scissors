784db9e0e9e20237302b5884efc4c13dfff4e09e
gayatri-tonde <gayatritonde7@gmail.com>
2025-01-24 23:54:45 +0530

    create functions

--- javascript.js
+++ javascript.js
@@ -15,29 +15,55 @@ function getUserChoice() {
     let userChoice = prompt("Enter rock, paper or scissor");
     return userChoice;
 }
-
-const compChoice = getCompChoice();
-const userChoice = getUserChoice();
+let userScore = 0;
+let compScore = 0;
 
 function playRound(compChoice, userChoice) {
+    let compChoice = getCompChoice();
+    let userChoice = getUserChoice();
+    let result = "You Win.... " + userChoice + " beats " + compChoice + " !!";
     if (compChoice == userChoice) {
         return "tie";
     }
-
-    else if (compChoice = "rock" && userChoice = "paper") {
-
+    if ((compChoice = "rock") && (userChoice = "paper")) {
+        userScore++;
+        return result;
     }
-
-    else if (compChoice = "paper") {
-
+    else if ((compChoice = "paper") && (userChoice = "scissor")) {
+        userScore++;
+        return result;
     }
-
-    else if (compChoice = "scissor") { 
-
+    else if ((compChoice = "scissor") && (userChoice = "rock")) {
+        userScore++;
+        return result;
     }
-
     else {
-        return "YOU LOST...!";
+        compScore++;
+        return "YOU LOST... " + compChoice + " beats " + userChoice + " !!";
+    }    
+}
+
+function playGame(){
+    let rounds=5;
+    for(i=0;i<=rounds;i++)
+    {
+        playGame();
+        print("player score"+"    " + "computer score");
+        print(userScore +"    "+ compScore);
+        if(userScore==compScore)
+        {
+            return "TIE..!";
+        }
+        else if (userScore<compScore)
+        {
+            return "YOU LOST...!"
+        }
+        else 
+        {
+            return "YOU WON...!"
+        }
     }
 }
+
+
 

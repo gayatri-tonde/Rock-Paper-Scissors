43edc67ed10b85cb08f9263b558eec29d0883142
Gayatri <gayatritonde7@gmail.com>
2025-07-05 23:29:12 +0530

    ADD CSS

--- script.js
+++ script.js
@@ -8,12 +8,19 @@ function getComputerChoice(){
     console.log("computer choice: ", compChoice);
     return compChoice;
 } 
-
+alert("RULES:\n" +
+    "1. Each game will have 5 rounds.\n" +
+    "2. In each round player must select any one button as choice.\n" +
+    "3. If it is a tie, then the round will be played again.\n" +
+    "4. After each round the score will be displayed.");
+    
 function playround(userChoice) {
+
     const compChoice = getComputerChoice();
     document.querySelector("#compChoice").innerHTML = "Choice: " + compChoice;
+
     if (userChoice == compChoice) {
-        
+        alert("It's a tie! Play again.");   
     }
     else {
         if (compChoice == "rock" && userChoice == "paper" ||
@@ -36,17 +43,18 @@ function playround(userChoice) {
 
     if (round == 5) {
         document.querySelectorAll(".btn").forEach(btn => btn.disabled = true);
-        result(userScore, compScore);
+        setTimeout(() => {
+            result(userScore, compScore);
+        }, 100); // 100ms delay lets the browser update the UI
     }
 }
 
 function result(userScore, compScore) {
-    const result = document.querySelector("#result")
     if (userScore > compScore) {
-        result.innerHTML="YOU WON!!!"
+        alert("YOU WON!!!")
     }
     else {
-       result.innerHTML="COMPUTER WON!!!"
+       alert("COMPUTER WON!!!")
     }
 }
 
